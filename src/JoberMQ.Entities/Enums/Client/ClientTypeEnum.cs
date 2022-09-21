00a0d2c2fbf384857e0be0b0bc1af7213e8fb2dc
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2022-09-22 02:31:24 +0300

    clien operation completed

---
+++ src/JoberMQ.Entities/Enums/Client/ClientTypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace JoberMQ.Entities.Enums.Client
+{
+    public enum ClientTypeEnum
+    {
+        Normal = 1,
+        LoadBalancing = 2,
+        HighAvailability = 3,
+        LoadBalancingANDHighAvailability = 4
+    }
+}
