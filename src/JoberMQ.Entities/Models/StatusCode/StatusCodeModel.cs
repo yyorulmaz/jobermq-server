58b73fe2abfb8afbc8980eca1db6601d2f6b3db4
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2022-09-21 06:17:11 +0300

    status code ok

---
+++ src/JoberMQ.Entities/Models/StatusCode/StatusCodeModel.cs
@@ -0,0 +1,13 @@
+﻿using JoberMQ.Entities.Enums.StatusCode;
+using System.Collections.Generic;
+
+namespace JoberMQ.Entities.Models.StatusCode
+{
+    public class StatusCodeModel
+    {
+        //[JsonConverter(typeof(StringEnumConverter))]
+        public StatusCodeTypeEnum StatusCodeType { get; set; }
+        public string StatusCode { get; set; }
+        public List<StatusCodeMessageModel> StatusCodeMessages { get; set; }
+    }
+}
---
+++ src/statuscode/DefaultStatusCode.cs
@@ -0,0 +1,49 @@
+﻿using GenRep.ConcurrentRepository.ConcurrentDictionary;
+using JoberMQ.Entities.Enums.StatusCode;
+using JoberMQ.Entities.Models.StatusCode;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+internal class DefaultStatusCode : ConcurrentDictionaryRepository<string, StatusCodeModel>, IStatusCode
+{
+    private StatusCodeMessageLanguageEnum statusCodeMessageLanguage;
+    public DefaultStatusCode(StatusCodeMessageLanguageEnum statusCodeMessageLanguage) => this.statusCodeMessageLanguage = statusCodeMessageLanguage;
+
+    public bool Start()
+    {
+        try
+        {
+            var ass = Assembly.GetAssembly(typeof(DefaultStatusCode));
+            var data = ResourceFileHelper.GetString(ass, @"StatusCode", "StatusCodes.json");
+            var deserialize = JsonConvert.DeserializeObject<List<StatusCodeModel>>(data);
+
+            foreach (var item in deserialize)
+                Add(item.StatusCode, item);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public string GetStatusMessage(string statusCode) => GetStatusMessage(statusCode, statusCodeMessageLanguage);
+    public string GetStatusMessage(string statusCode, StatusCodeMessageLanguageEnum language)
+    {
+        try
+        {
+            var status = Get(statusCode);
+
+            var message = status.StatusCodeMessages.FirstOrDefault(x => x.Language == language).Message;
+            return $"{status.StatusCode} - {message}";
+        }
+        catch (Exception)
+        {
+            return null;
+        }
+    }
+}
