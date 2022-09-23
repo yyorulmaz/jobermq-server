1e30d970ddcff376ee3b69dd1bd28b9e96006a8b
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2022-09-23 17:47:06 +0300

    continues

---
+++ src/JoberMQ.Server/Abstraction/Queue/IQueueChildDataConcurrentQueue.cs
@@ -0,0 +1,32 @@
+﻿using JoberMQ.Entities.Dbos;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace JoberMQ.Server.Abstraction.Queue
+{
+    internal interface IQueueChildDataConcurrentQueue
+    {
+        #region Data
+        IQueueMainData DataMain { get; }
+        ConcurrentQueue<MessageDbo> Data { get; }
+        #endregion
+
+        #region Count
+        int Count { get; }
+        #endregion
+
+        #region CRUD
+        MessageDbo Get();
+        public ConcurrentQueue<MessageDbo> GetAll();
+        bool Add(MessageDbo value);
+        MessageDbo Remove(Guid key);
+        #endregion
+
+        #region Changed
+        event Action<MessageDbo> ChangedAdded;
+        event Action<MessageDbo> ChangedRemoved;
+        #endregion
+    }
+}
---
+++ src/JoberMQ.Server/Implementation/Queue/DfQueueChildDataConcurrentStack.cs
@@ -0,0 +1,72 @@
+﻿using JoberMQ.Entities.Dbos;
+using JoberMQ.Server.Abstraction.Queue;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JoberMQ.Server.Implementation.Queue
+{
+    internal class DfQueueChildDataConcurrentStack : IQueueChildDataConcurrentStack
+    {
+        #region Constructor
+        public DfQueueChildDataConcurrentStack(IQueueMainData dataMain)
+        {
+            this.dataMain = dataMain;
+            this.data = new ConcurrentStack<MessageDbo>();
+        }
+        #endregion
+
+        #region Data
+        private readonly IQueueMainData dataMain;
+        public IQueueMainData DataMain => dataMain;
+
+
+        private readonly ConcurrentStack<MessageDbo> data;
+        public ConcurrentStack<MessageDbo> Data => data;
+        #endregion
+
+        #region Count
+        public int Count => data.Count;
+        #endregion
+
+        #region CRUD
+        public MessageDbo Get()
+        {
+            data.TryPeek(out var value);
+            return value;
+        }
+        public ConcurrentStack<MessageDbo> GetAll()
+        {
+            return data;
+        }
+        public bool Add(MessageDbo value)
+        {
+            try
+            {
+                dataMain.Add(value.Id, value);
+                data.Push(value);
+                ChangedAdded?.Invoke(value);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public MessageDbo Remove(Guid key)
+        {
+            dataMain.Remove(key);
+            var result = data.TryPeek(out var value);
+            if (result)
+                ChangedRemoved?.Invoke(value);
+            return value;
+        }
+        #endregion
+
+        #region Changed
+        public event Action<MessageDbo> ChangedAdded;
+        public event Action<MessageDbo> ChangedRemoved;
+        #endregion
+    }
+}
