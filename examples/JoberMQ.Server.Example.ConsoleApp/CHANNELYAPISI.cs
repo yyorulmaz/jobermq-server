db92c53dc5fdf5ab3ab74b6b8208ad8b57c5d304
yusuf yorulmaz <yusufyorulmaz@gmail.com>
2023-01-11 00:30:10 +0300

    lazım olabilecek kodlar

---
+++ examples/JoberMQ.Server.Example.ConsoleApp/CHANNELYAPISI.cs
@@ -0,0 +1,72 @@
+﻿//using System.Threading.Channels;
+
+//var channel = Channel.CreateUnbounded<int>();
+////var channel = Channel.CreateBounded<string>(1);
+////var channel = Channel.CreateUnbounded<byte>();
+
+
+
+////Producer
+//_ = Task.Run(async () =>
+//{
+//int number = 1;
+//for (int i = 0; i < 10; i++)
+//{
+//await channel.Writer.WriteAsync(number);
+//number++;
+//await Task.Delay(1000);
+//}
+//});
+
+
+////Consumer
+//_ = Task.Run(async () =>
+//{
+//while (true)
+//{
+
+//var data = await channel.Reader.ReadAsync();
+//Console.WriteLine($"Data:{data}");
+////await Task.Delay(3000);
+//}
+//});
+
+////Consumer
+//_ = Task.Run(async () =>
+//{
+//while (true)
+//{
+//var data = await channel.Reader.ReadAsync();
+//Console.WriteLine($"Data:{data} hhh");
+////await Task.Delay(3000);
+//}
+//});
+
+////Consumer
+//_ = Task.Run(async () =>
+//{
+//while (true)
+//{
+//var data = await channel.Reader.ReadAsync();
+//Console.WriteLine($"Data:{data} zzz");
+////await Task.Delay(3000);
+//}
+//});
+
+//Console.ReadLine();
+
+//public class AAAAA
+//{
+//    static void Deneme()
+//    {
+
+//    }
+//    static void Deneme(int aaaaa)
+//    {
+
+//    }
+//    static int Deneme(int aaaaa, int bbbbb)
+//    {
+//        return 5;
+//    }
+//} 
--- examples/JoberMQ.Server.Example.ConsoleApp/Program.cs
+++ examples/JoberMQ.Server.Example.ConsoleApp/Program.cs
@@ -1,102 +1,4 @@
-﻿//using System.Threading.Channels;
-
-//var config = JoberMQ.Server.Factory.GetServerConfig();
-//var server = JoberMQ.Server.Factory.CreateServer(config);
-//server.Start();
-
-
-//var startDate = DateTime.Now;
-//for (int i = 0; i < 1000000; i++)
-//{
-//    //Console.WriteLine(i);
-//}
-//var endDate = DateTime.Now;
-
-//Console.WriteLine(endDate - startDate);
-
-
-
-
-#region CHANNEL YAPISI
-//var channel = Channel.CreateUnbounded<int>();
-////var channel = Channel.CreateBounded<string>(1);
-////var channel = Channel.CreateUnbounded<byte>();
-
-
-
-////Producer
-//_ = Task.Run(async () =>
-//{
-//int number = 1;
-//for (int i = 0; i < 10; i++)
-//{
-//await channel.Writer.WriteAsync(number);
-//number++;
-//await Task.Delay(1000);
-//}
-//});
-
-
-////Consumer
-//_ = Task.Run(async () =>
-//{
-//while (true)
-//{
-
-//var data = await channel.Reader.ReadAsync();
-//Console.WriteLine($"Data:{data}");
-////await Task.Delay(3000);
-//}
-//});
-
-////Consumer
-//_ = Task.Run(async () =>
-//{
-//while (true)
-//{
-//var data = await channel.Reader.ReadAsync();
-//Console.WriteLine($"Data:{data} hhh");
-////await Task.Delay(3000);
-//}
-//});
-
-////Consumer
-//_ = Task.Run(async () =>
-//{
-//while (true)
-//{
-//var data = await channel.Reader.ReadAsync();
-//Console.WriteLine($"Data:{data} zzz");
-////await Task.Delay(3000);
-//}
-//});
-
-//Console.ReadLine();
-
-//public class AAAAA
-//{
-//    static void Deneme()
-//    {
-
-//    }
-//    static void Deneme(int aaaaa)
-//    {
-
-//    }
-//    static int Deneme(int aaaaa, int bbbbb)
-//    {
-//        return 5;
-//    }
-//} 
-#endregion
-
-
-
-
-
-
-
-#region senaryo 1
+﻿#region senaryo 1
 ////var jober = JoberMQ.JoberHost
 ////.CreateDefaultBuilder()
 ////.Build();
@@ -110,6 +12,7 @@ using JoberMQ;
 using JoberMQ.Common.Enums.Configuration;
 using JoberMQ.Extensions;
 using System;
+using System.Collections.Generic;
 
 var configuration = JoberHost.CreateConfiguration();
 
